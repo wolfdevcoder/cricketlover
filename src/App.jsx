0a61e24c1fa6f7415a497b21be5d0fed86bd1a9d
Devansh <devansh.docthub@gmail.com>
2023-11-13 18:41:17 +0100

    commit for cricket lover

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <iframe
+        title="My Embedded Content"
+        src="https://crichdstreaming.xyz/embed2.php?id=starsp3"
+        width="100%"
+        height="540"
+        frameBorder="0"
+        scrolling="no"
+        allowFullScreen={true}
+        marginHeight="0"
+        marginWidth="0"
+        webkitallowfullscreen="true"
+        mozallowfullscreen="true"
+        allow="fullscreen"
+      ></iframe>
+    </>
+  );
+}
+
+export default App;
